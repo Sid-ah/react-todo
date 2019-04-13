5b8a197d180acff76f10262c863080e6c9cc3ea1
sid-ah <sidah_10@hotmail.com>
2019-04-13 14:11:53 -0400

    Complete Browser router

--- src/App.js
+++ src/App.js
@@ -1,36 +1,36 @@
-import React, { Component } from 'react';
-import './App.css';
-import Login from './Login'
-import Header from './Header'
-import { HashRouter, Route, Switch, Link } from 'react-router-dom'
+  import React, { Component } from 'react';
+  import './App.css';
+  import Login from './Login'
+  import Header from './Header'
+  import { BrowserRouter, Route, Switch, Link } from 'react-router-dom'
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {       
+  class App extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {       
+      }
+      console.log('inside the constructor');
     }
-    console.log('inside the constructor');
+
+    componentDidMount() {
+      console.log('Component just mounted')
+    }
+
+    render() {
+      console.log('Rendering my component')
+      return (
+        <div className="App">
+          <header className="App-header">
+          <BrowserRouter>
+            <Header />
+            <Switch>
+              <Route exact path="/login" component={Login} />         
+            </Switch>
+          </BrowserRouter>
+          </header>
+        </div>
+      );
+    }
   }
 
-  componentDidMount() {
-    console.log('Component just mounted')
-  }
-
-  render() {
-    console.log('Rendering my component')
-    return (
-      <div className="App">
-        <header className="App-header">
-        <HashRouter>
-          <Header />
-          <Switch>
-            <Route exact path="/login" component={Login} />         
-          </Switch>
-        </HashRouter>
-        </header>
-      </div>
-    );
-  }
-}
-
-export default App;
+  export default App;
