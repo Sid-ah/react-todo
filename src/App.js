d73c226402d6fd6a599ca93f2ecfc26bcce4f5df
sid-ah <sidah_10@hotmail.com>
2019-04-14 02:43:43 -0400

    Add redux to app.js

--- src/App.js
+++ src/App.js
@@ -1,89 +1,96 @@
-  import React, { Component } from 'react';
-  import './App.css';
-  import Login from './Login'
-  import Landing from './Landing'
-  import { BrowserRouter, Route, Switch, Link, Redirect } from 'react-router-dom'
-  import Protected from './Protected'
-  import {firebaseAuth} from './utils/auth'
+import React, { Component } from 'react';
+import './App.css';
+import Login from './Login'
+import Landing from './Landing'
+import { BrowserRouter, Route, Switch, Link, Redirect } from 'react-router-dom'
+import Protected from './Protected'
+import {firebaseAuth} from './utils/auth'
+import {connect} from 'react-redux';
+import {isAuthenticated} from './actions/index';
 
-  const baseUrl = process.env.PUBLIC_URL; 
-  const fourOhFour = <h1>404 We are out, so should you be!</h1>
-  class App extends Component {
-    constructor(props) {
-      super(props);
-      this.state = {       
-        authenticated: false
-      }
-      this.privateRoute = this.privateRoute.bind (this);
-      this.authStateChanged = this.authStateChanged.bind (this);
-      this.loginRoute = this.loginRoute.bind (this);
-      console.log('inside the constructor');
+const baseUrl = process.env.PUBLIC_URL; 
+const fourOhFour = <h1>404 We are out, so should you be!</h1>
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {       
+      authenticated: false
     }
-
-    componentDidMount () {
-      console.log('Component just mounted')
-      this.removeListener = firebaseAuth ().onAuthStateChanged (
-        this.authStateChanged
-      );
-    }
-    authStateChanged (user) {
-      if (user) {
-        const {uid} = firebaseAuth ().currentUser;
-        console.log(uid)
-        if (uid) {
-          console.log('about to set isAuthenticated to true')
-          this.setState ({authenticated: true});
-        } else {
-          firebaseAuth ().signOut ();
-          this.setState ({authenticated: false});
-       }
-      } else {
-        firebaseAuth ().signOut ();
-        this.setState ({authenticated: false});
-      }
-    }
-    privateRoute({component: Component, authenticated, ...rest}) {
-      return (
-        <Route
-          {...rest}
-          render={props =>
-            authenticated === true
-              ? <Component {...props} />
-              : <Redirect to={`${baseUrl}/login`} />}
-        />
-      );
-    }
-    loginRoute({component: Component, authenticated, ...rest}) {
-      console.log (`this.state.authenticated ${this.state.authenticated}`);
-      return (
-        <Route
-          {...rest}
-          render={props =>
-            authenticated === true
-              ? <Redirect to={`${baseUrl}/protected`} />
-              : <Component {...props} />}
-        />
-      );
-    }
-
-    render() {
-      console.log('Rendering my component')
-      const PrivateRoute = this.privateRoute;
-      const LoginRoute = this.loginRoute;
-      const {authenticated} = this.state
-      return (
-        <BrowserRouter>
-          <div className="app">
-            <Switch>
-              <PrivateRoute path={`${baseUrl}/protected`} authenticated={authenticated} component={Protected} />
-              <LoginRoute path={`${baseUrl}/login`} authenticated={authenticated} component={Login} />
-              <Route exact path={`${baseUrl}/`} component={Landing} />
-              <Route component={fourOhFour} />
-            </Switch>
-          </div>
-        </BrowserRouter>
-      );
-    }
+    this.privateRoute = this.privateRoute.bind (this);
+    this.authStateChanged = this.authStateChanged.bind (this);
+    this.loginRoute = this.loginRoute.bind (this);
+    console.log('inside the constructor');
   }
 
-  export default App;
+  componentDidMount () {
+    console.log('Component just mounted')
+    this.removeListener = firebaseAuth ().onAuthStateChanged (
+      this.authStateChanged
+    );
+  }
+  authStateChanged (user) {
+    if (user) {
+      const {uid} = firebaseAuth ().currentUser;
+      console.log(uid)
+      if (uid) {
+        console.log('about to set isAuthenticated to true')
+        this.props.isAuthenticated ({authenticated: true});
+        this.setState ({authenticated: true});
+      } else {
+        firebaseAuth ().signOut ();
+        this.props.isAuthenticated ({authenticated: false});
+      }
+    } else {
+      firebaseAuth ().signOut ();
+      this.props.isAuthenticated ({authenticated: false});
+    }
+  }
+  privateRoute({component: Component, authenticated, ...rest}) {
+    return (
+      <Route
+        {...rest}
+        render={props =>
+          this.props.authenticated === true
+            ? <Component {...props} />
+            : <Redirect to={`${baseUrl}/login`} />}
+      />
+    );
+  }
+  loginRoute({component: Component, authenticated, ...rest}) {
+    console.log (`this.state.authenticated ${this.state.authenticated}`);
+    return (
+      <Route
+        {...rest}
+        render={props =>
+          this.props.authenticated === true
+            ? <Redirect to={`${baseUrl}/protected`} />
+            : <Component {...props} />}
+      />
+    );
+  }
+
+  render() {
+    console.log('Rendering my component')
+    const PrivateRoute = this.privateRoute;
+    const LoginRoute = this.loginRoute;
+    const {authenticated} = this.state
+    return (
+      <BrowserRouter>
+        <div className="app">
+          <Switch>
+            <PrivateRoute path={`${baseUrl}/protected`} authenticated={authenticated} component={Protected} />
+            <LoginRoute path={`${baseUrl}/login`} authenticated={authenticated} component={Login} />
+            <Route exact path={`${baseUrl}/`} component={Landing} />
+            <Route component={fourOhFour} />
+          </Switch>
+        </div>
+      </BrowserRouter>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  authenticated: state.authenticated,
+});
+
+export default connect (mapStateToProps, {isAuthenticated}) (App);
--- src/index.js
+++ src/index.js
@@ -2,9 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import reducer from './reducers';
+import {Provider} from 'react-redux'
+import {createStore} from 'redux'
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore (
+  reducer,
+  {authenticated: false},
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__ ()
+);
+
+ReactDOM.render (
+  <Provider store={store}><App /></Provider>,
+  document.getElementById ('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
